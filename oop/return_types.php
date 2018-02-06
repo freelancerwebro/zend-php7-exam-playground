3600c9c5d24e45e1a33dca46d325c24c9301f728
Sorin Marian <marian.sorin@yahoo.com>
2018-02-06 21:34:57 +0200

    implement return types in php 7

--- oop/return_types.php
+++ oop/return_types.php
@@ -1,2 +1,41 @@
 <?php
 
+// works on PHP 7+ only 
+//
+
+declare(strict_types=1);
+
+$a = 10;
+$b = 22;
+
+function calculateSum(int $a, int $b): int{
+	return $a + $b;
+}
+
+print_r(calculateSum($a, $b));
+
+
+
+
+$x = "Hello";
+$y = "World";
+
+function displayHelloWorld(string $x, string $y): string{
+	return $x.$y;
+}
+
+print_r(displayHelloWorld($x, $y));
+
+
+
+
+$x = 1;
+$y = "World";
+
+function displayOneWorld(string $x, string $y): string{
+	return $x.$y;
+}
+
+// PHP Fatal error:  Uncaught TypeError: Argument 1 passed to displayOneWorld() must be of the type string, integer given
+print_r(displayOneWorld($x, $y));
+
