451d0d8dcfcf77c342c3c04bcc87453b9046f2a0
Sorin <marian.sorin@yahoo.com>
2018-02-11 15:40:35 +0200

    implement type hinting

---
+++ oop/type_hinting.php
@@ -0,0 +1,101 @@
+<?php
+
+// 1) array type hinting: available starting with PHP 5
+// 
+
+function displayColors(array $colors)
+{
+	foreach($colors as $color)
+	{
+		echo $color."\r\n";
+	}
+}
+
+//displayColors("white");
+
+// result:
+//    Fatal error: Uncaught TypeError: Argument 1 passed to displayColors() must be of the type array, string given
+
+$colors = array("green", "yellow", "blue");
+displayColors($colors);
+
+//result:
+//   green
+//   yellow
+//   blue
+
+
+
+
+
+
+// 2) object type hinting - available starting with PHP 5
+//
+
+class Model{
+
+	protected $database;
+
+
+	public function __construct(Database $database)
+	{
+		$this->database = $database;
+	}
+}
+
+class Database{
+
+}
+
+$database = new Database();
+$model = new Model($database);
+
+
+
+
+
+
+// 3) scalar type hinting - available starting with PHP 7
+//
+
+class Product{
+
+	protected $name;
+	protected $price;
+	protected $code;
+	protected $active;
+
+	// string type hinting
+	public function setName(string $name)
+	{
+		$this->name = $name;
+	}
+
+	// float type hinting
+	public function setPrice(float $price)
+	{
+		$this->price = $price;
+	}
+
+	// integer type hinting
+	public function setCode(int $code)
+	{
+		$this->code = $code;
+	}
+
+	// boolean type hinting
+	public function setActive(bool $active)
+	{
+		$this->active = $active;
+	}
+
+}
+
+
+
+
+// 4) interface type hinting
+//
+
+
+
